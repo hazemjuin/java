2303366ae5373ce4e2350cf8559cceb0e4457875
hazem juini <hazemjuini243@gmail.com>
2023-10-02 12:31:02 +0200

    first java

---
+++ W01-Day-01/practice/first_java/MyInfo.java
@@ -0,0 +1,13 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        // Replace these with your own information
+        String name = "Your Name";
+        int age = 25; // Your age
+        String hometown = "Your Hometown";
+
+        // Print your information on different lines
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hometown: " + hometown);
+    }
+}
