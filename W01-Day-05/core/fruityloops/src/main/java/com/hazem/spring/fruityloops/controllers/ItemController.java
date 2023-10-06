76f35c442b336e3653a6f04f11ff253281a22c58
hazem juini <hazemjuini243@gmail.com>
2023-10-06 14:19:41 +0200

    fruity loops

---
+++ W01-Day-05/core/fruityloops/src/main/java/com/hazem/spring/fruityloops/controllers/ItemController.java
@@ -0,0 +1,27 @@
+package com.hazem.spring.fruityloops.controllers;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hazem.spring.fruityloops.models.Item;
+
+
+@Controller
+public class ItemController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        ArrayList<Item> fruits = new ArrayList<>();
+        fruits.add(new Item("Kiwi", 1.5));
+        fruits.add(new Item("Mango", 2.0));
+        fruits.add(new Item("Goji Berries", 4.0));
+        fruits.add(new Item("Guava", 0.75));
+
+        // Add fruits to the model
+        model.addAttribute("fruits", fruits);
+
+        return "index.jsp";
+    }
+}
---
+++ W01-Day-05/core/fruityloops/src/test/java/com/hazem/spring/fruityloops/FruityloopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hazem.spring.fruityloops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruityloopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
